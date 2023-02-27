b3c9e6739479b4c93430870a36ad6ac8c4637d75
Rosario Cafaro <cafaro.rosario@gmail.com>
2023-02-27 17:07:30 +0100

    added tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,9 @@
 export default function Index() {
     return (
-        <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.4"}}>
-            <h3>index.tsx</h3>
+        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+            <div className="text-blue-700">
+                <h3>index.tsx</h3>
+            </div>
         </div>
     );
 }
